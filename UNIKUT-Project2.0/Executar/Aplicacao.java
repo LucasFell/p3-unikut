b47da4d91cdfae603467f34cac850fc1a8b48a3a
Lucas Felinto <49879432+LucasFell@users.noreply.github.com>
2021-04-01 21:28:01 -0300

    Implementamos a edição de perfil
    A adição de amigos está sendo desenvolvida.

---
+++ UNIKUT-Project2.0/Executar/Aplicacao.java
@@ -0,0 +1,284 @@
+package Executar;
+
+import java.util.Scanner;
+import Logica.*;
+
+public class Aplicacao {
+
+    /*
+    * @author Lucas
+     */
+    public static void menuAddAmigos() {
+        System.out.println("\nAdicionar Amigos");
+        System.out.println("1 => Adicionar Amigo");
+        System.out.println("2 => Sair");
+        System.out.print("=>");
+    }
+
+    public static void menuAlterar() {
+        System.out.println("\nAlterar Conta");
+        System.out.println("1 => Editar Apelido");
+        System.out.println("2 => Editar Senha");
+        System.out.println("3 => Sair");
+        System.out.print("=>");
+    }
+
+    public static void menuMain() {
+        System.out.println("Seu Feed:\n");
+        System.out.println("1 => Editar Perfil");
+        System.out.println("2 => Ver Amigos");
+        System.out.println("3 => Adicionar Amigos");
+        System.out.println("4 => Enviar Recados");
+        System.out.println("5 => Ver Solicitacoes de Amizade");
+        System.out.println("6 => Ver Recados");
+        System.out.println("10 => para Sair");
+        System.out.print("=>");
+    }
+
+    public static void menuStart() {
+        System.out.println("\nEh novo por aqui?");
+        System.out.println("1 => Criar Nova Conta\n");
+
+        System.out.println("Ja tem cadastro?");
+        System.out.println("2 => Entrar na sua conta\n");
+
+        System.out.println("Quer Nos Abandonar?");
+        System.out.println("3 => para Sair");
+        System.out.print("=>");
+    }
+
+    public static void main(String[] args) {
+        Unikut social = new Unikut();
+
+        Scanner s = new Scanner(System.in);
+        String login, nickName, password, passwordConfirm;
+        String loginStart, nickNameStart, passwordStart;
+
+        loginStart = "Conta Mestra";
+        nickNameStart = "Conta Mestra";
+        passwordStart = "Conta Mestra";
+        social.criarConta(loginStart, nickNameStart, passwordStart);
+
+        System.out.println("Bem vindo ao UNIKUT!\n");
+
+        menuStart();
+        int opcao = s.nextInt();
+        s.nextLine();
+        while (opcao != 3) {
+            switch (opcao) {
+                case 1 -> {
+                    //Criar Nova Conta
+                    System.out.print("\nCriar Login: ");
+                    login = s.nextLine();
+                    // Verifica se o login ja existe
+                    while (social.verificaLogin(login)) {
+                        System.out.print("\nErro:\nLogin Ja Existente\nPor Favor criar outro Login: ");
+                        login = s.nextLine();
+                    }
+
+                    System.out.print("Criar Apelido (Opcional): ");
+                    nickName = s.nextLine();
+
+                    System.out.print("\nCrie uma senha: ");
+                    password = s.nextLine();
+
+                    System.out.print("\nDigite a senha novamente: ");
+                    passwordConfirm = s.nextLine();
+                    // Varifica se as senhas estão iguais
+                    while (!password.equals(passwordConfirm)) {
+                        System.out.print("\nSuas senhas nao estao compativeis\nDigite novamente a senha: ");
+                        password = s.next();
+                        System.out.print("\nVerifique a senha: ");
+                        passwordConfirm = s.next();
+                    }
+                    // Se Nao inserir o nome, o mesmo recebe "Convidado"
+                    if (nickName.isEmpty() == true) {
+                        nickName = "Convidado!";
+                    }
+                    System.out.println("\nBem Vindo Ao UNIKUT!");
+                    System.out.println(nickName + "!");
+                    System.out.println("Conta criada com sucesso...");
+
+                    //Criando a conta com os atributos coletados
+                    social.criarConta(login, nickName, password);
+                    System.out.println("\nBem vindo " + login + "!");
+                    menuMain();
+                    int opcao2 = s.nextInt();
+                    s.nextLine();
+                    while (opcao2 != 10) {
+                        switch (opcao2) {
+                            case 1 -> { // Editar perfil
+                                menuAlterar();
+                                int opcao3 = s.nextInt();
+                                s.nextLine();
+                                while (opcao3 != 3) {
+                                    switch (opcao3) {
+                                        case 1 -> { // Alterar Apelido
+                                            System.out.println("\nNovo Apleido:");
+                                            String newNickName = s.nextLine();
+                                            social.alterarContaNome(login, newNickName);
+                                            social.exibir();
+
+                                            System.out.println("\nApelido alterado com sucesso!");
+                                        }
+                                        case 2 -> { // Alterar Senha
+                                            System.out.println("\nNova Senha:");
+                                            String newPassword = s.nextLine();
+                                            social.alterarContaSenha(login, newPassword);
+                                            social.exibir();
+
+                                            System.out.println("\nSenha alterada com sucesso!");
+                                        }
+                                        default ->
+                                            System.out.println("\nComando inválido");
+                                    }
+
+                                    menuAlterar();
+                                    opcao3 = s.nextInt();
+                                    s.nextLine();
+                                }
+
+                            }
+                            case 2 -> { // Exibir amigos
+                                social.exibir();
+                            }
+                            case 3 -> { // Adicionar amigos
+                                menuAddAmigos();
+                                int opcao4 = s.nextInt();
+                                while (opcao4 != 2) {
+                                    switch (opcao4) {
+                                        case 1 -> {
+                                            System.out.print("\nDigite o Login do seu Amigo: ");
+                                            String loginAmigo = s.nextLine();
+                                            while (!social.verificaLogin(loginAmigo)) {
+                                                System.out.print("\nErro:\nLogin Não Existente\nPor Favor usar outro Login: ");
+                                                loginAmigo = s.nextLine();
+                                            }
+
+                                        }
+                                    }
+
+                                }
+                            }
+                            case 4 -> { // Enviar recados
+
+                            }
+                            case 5 -> { // Ver Solicitaçoes de Amizade
+
+                            }
+                            case 6 -> { // Ver Recados
+
+                            }
+                            default ->
+                                System.out.println("\nComando inválido");
+                        }
+
+                        System.out.println("\nBem vindo " + login + "!");
+                        menuMain();
+                        opcao2 = s.nextInt();
+                        s.nextLine();
+                    }
+                }
+                case 2 -> {
+                    //Login
+                    System.out.print("\nLogin: ");
+                    login = s.nextLine();
+                    while (!social.verificaLogin(login)) {
+                        System.out.print("\nErro:\nLogin Não Existente\nPor Favor usar outro Login: ");
+                        login = s.nextLine();
+                    }
+                    System.out.print("Senha: ");
+                    password = s.nextLine();
+                    while (!social.verificaSenha(login, password)) {
+                        System.out.print("\nErro:\nSenha Incorreta\n\nPor Favor tente novamente: ");
+                        password = s.nextLine();
+                    }
+                    System.out.println("\nBem vindo " + login + "!");
+                    menuMain();
+                    int opcao2 = s.nextInt();
+                    s.nextLine();
+                    while (opcao2 != 10) {
+                        switch (opcao2) {
+                            case 1 -> { // Editar perfil
+                                menuAlterar();
+                                int opcao3 = s.nextInt();
+                                s.nextLine();
+                                while (opcao3 != 3) {
+                                    switch (opcao3) {
+                                        case 1 -> { // Alterar Apelido
+                                            System.out.println("\nNovo Apleido:");
+                                            String newNickName = s.nextLine();
+                                            social.alterarContaNome(login, newNickName);
+
+                                            System.out.println("\nApelido alterado com sucesso!");
+                                        }
+                                        case 2 -> { // Alterar Senha
+                                            System.out.println("\nNova Senha:");
+                                            String newPassword = s.nextLine();
+                                            social.alterarContaSenha(login, newPassword);
+
+                                            System.out.println("\nSenha alterada com sucesso!");
+                                        }
+                                        default ->
+                                            System.out.println("\nComando inválido");
+                                    }
+
+                                    menuAlterar();
+                                    opcao3 = s.nextInt();
+                                    s.nextLine();
+                                }
+                            }
+                            case 2 -> { // Exibir amigos
+                                social.exibir();
+                            }
+                            case 3 -> { // Adicionar amigos
+                                menuAddAmigos();
+                                int opcao4 = s.nextInt();
+                                while (opcao4 != 2) {
+                                    switch (opcao4) {
+                                        case 1 -> {
+                                            System.out.print("\nDigite o Login do seu Amigo: ");
+                                            String loginAmigo = s.nextLine();
+                                            while (!social.verificaLogin(loginAmigo)) {
+                                                System.out.print("\nErro:\nLogin Não Existente\nPor Favor usar outro Login: ");
+                                                loginAmigo = s.nextLine();
+                                            }
+
+                                        }
+                                    }
+
+                                }
+                            }
+                            case 4 -> { // Enviar recados
+
+                            }
+                            case 5 -> { // Ver Solicitaçoes de Amizade
+
+                            }
+                            case 6 -> { // Ver Recados
+
+                            }
+                            default ->
+                                System.out.println("\nComando inválido");
+                        }
+
+                        System.out.println("\nBem vindo " + login + "!");
+                        menuMain();
+                        opcao2 = s.nextInt();
+                        s.nextLine();
+                    }
+                }
+                default ->
+                    System.out.println("\nComando inválido");
+
+            }
+
+            System.out.println("\nCerto...");
+            menuStart();
+            opcao = s.nextInt();
+            s.nextLine();
+        }
+        System.out.println("Adeus :( ");
+    }
+
+}
